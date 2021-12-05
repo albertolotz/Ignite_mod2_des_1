d01a32fcbe1aa25a371148e3906cba66ceb1b07f
albertolotz <alberto10lotz@gmail.com>
2021-12-05 17:37:09 -0300

    first commit

---
+++ lib/flightex.ex
@@ -0,0 +1,10 @@
+defmodule Flightex do
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+end
